4d85242d009694f1207e439b9680641f1344aa88
Adalberto Maia <btowmaia@gmail.com>
2021-03-30 09:23:54 -0300

    paginator done

--- resgatesoldado/pages/movies.js
+++ resgatesoldado/pages/movies.js
@@ -2,24 +2,37 @@ import { useState } from 'react'
 import useSWR from 'swr'
 export default function Movies(){
     const [filmes, setFilmes]= useState('resgate')
-    const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=fd6b0637&s=${filmes}`,Fetcher)
+    const [page, setPage] = useState(1)
+    const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=fd6b0637&s=${filmes}&page=${page}`,Fetcher)
     
 
     const handleChange = e => {
         setFilmes(e.target.value)
     }
-
+    console.log(page)
     console.log(data)
     return (
         <div>
-            <section>
+            <div>
                 <input type="search" onChange={handleChange}/>
-                
-                {data && data.Search ? <div>{data.Search.map( (m) => <div>{m.Title} --- <br /><img src={m.Poster} alt="POSTER FALTANDO"></img> --- {m.Year}</div>  ) }</div> : <p>NÃO FORAM ENCONTRADOS FILMES</p>}
-                <nav>
-                    <ul id="paginator"></ul>
-                </nav>                
-            </section>
+                <div>
+                    <button onClick={() => setPage(page+1)}>Proxima Pagina</button>
+                    <button onClick={() => setPage(page-1)}>Proxima Anterior</button>
+                </div>
+                {data && 
+                    data.Search ? <div>{
+                        data.Search.map( 
+                            (m) => 
+                                <div>
+                                    {m.Title} --- <br />
+                                    <img src={m.Poster} alt="POSTER FALTANDO"></img>
+                                    --- {m.Year}
+                                 </div>  
+                                 )}</div> 
+                                : <p>NÃO FORAM ENCONTRADOS FILMES</p>}
+                      
+            </div>
+            
         </div>
     )
 }
@@ -29,7 +42,7 @@ async function Fetcher(url) {
     const res = await fetch(url)
 
     const json = await res.json()
-
+    
     return json
 
 }
