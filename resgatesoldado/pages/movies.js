591a2ff5c1b7f8f2d6cf240b180ab4271b5b0676
Adalberto Maiam <btowmaia@gmail.com>
2021-03-30 00:35:43 -0300

    fim do trabalho

--- resgatesoldado/pages/movies.js
+++ resgatesoldado/pages/movies.js
@@ -1,17 +1,27 @@
+import { useState } from 'react'
 import useSWR from 'swr'
 export default function Movies(){
+    const [filmes, setFilmes]= useState('resgate')
+    const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=fd6b0637&s=${filmes}`,Fetcher)
     
-    const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=fd6b0637&s=resgate`,getServerSideProps)
-    if (error) return <div>Falha na requisição...</div>
-    if (!data) return <div>Carregando...</div>
+
+    const handleChange = e => {
+        setFilmes(e.target.value)
+    }
+
+
     return (
         <div>
-             { data.Search.map( (m) => <div>{m.Title} --- <br /><img src={m.Poster} alt="POSTER FALTANDO"></img> --- {m.Year}</div>  ) }
+            <input type="search" onChange={handleChange}/>
+            
+            {data && data.Search ? <div>{data.Search.map( (m) => <div>{m.Title} --- <br /><img src={m.Poster} alt="POSTER FALTANDO"></img> --- {m.Year}</div>  ) }</div> : null}
+                                
+            
         </div>
     )
 }
 
-async function getServerSideProps(url) {
+async function Fetcher(url) {
 
     const res = await fetch(url)
 
